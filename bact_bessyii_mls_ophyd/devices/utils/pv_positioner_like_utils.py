4cbfb15835fe779db04625c2815e3016067d757f
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-02-16 18:05:45 +0100

    [FIX] ophyd-async > 0.15: Signal.set does not provide wait keyword

--- bact_bessyii_mls_ophyd/devices/utils/pv_positioner_like_utils.py
+++ bact_bessyii_mls_ophyd/devices/utils/pv_positioner_like_utils.py
@@ -35,7 +35,8 @@ class _SettableControllingDifference(StandardReadable, Movable, Stoppable, Stage
             self.units.get_value(),
             self.precision.get_value(),
         )
-        await self.setpoint.set(new_position, wait=False)
+        # Signal .set does not support wait any more ?
+        await self.setpoint.set(new_position)
         async for current_position in observe_value(
             self.readback, done_timeout=timeout
         ):
