70a68ccfa15642ccac490eac0d82a7563abd0f3c
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-03-03 10:27:37 +0100

    [TASK] reading bpm orbit table using epics_signal_r
    As a child of a device or directly using epics_signal_r

--- bact_bessyii_mls_ophyd/devices/raw/orbit.py
+++ bact_bessyii_mls_ophyd/devices/raw/orbit.py
@@ -1,31 +1,38 @@
-from typing import Annotated as A, Sequence
+from typing import Annotated as A
 
-import numpy as np
-from ophyd_async.core import SignalR, StandardReadable, Array1D
+from ophyd_async.core import SignalR, StandardReadable
 from ophyd_async.core import StandardReadableFormat as Format
-from ophyd_async.epics.core import EpicsDevice, PvSuffix
+from ophyd_async.epics.core import EpicsDevice, PvSuffix, epics_signal_r
+from ophyd_async.core import Table
 
-
-class Orbit(StandardReadable, EpicsDevice):
-    # fmt:off
-    names: A[ SignalR[ Sequence[str]       ], PvSuffix( "rdBpmNames" ), Format.CONFIG_SIGNAL   ]
-    spos:  A[ SignalR[ Array1D[np.float64] ], PvSuffix( "rdSPos"     ), Format.CONFIG_SIGNAL   ]
-
-    count: A[ SignalR[ int                 ], PvSuffix( "count"      ), Format.UNCACHED_SIGNAL ]
-
-    rpos:  A[ SignalR[ Array1D[np.float64] ], PvSuffix( "rdPos"      ), Format.UNCACHED_SIGNAL ]
-    btns:  A[ SignalR[ Array1D[np.float64] ], PvSuffix( "rdButtons"  ), Format.UNCACHED_SIGNAL ]
+class Orbit(StandardReadable):
     # fmt:on
+    # data: A[ SignalR [ Table ], PvSuffix( ":rdBPM"      ), Format.UNCACHED_SIGNAL ]
+
+     def __init__(self, prefix: str, *, name: str):
+        with self.add_children_as_readables():
+            self.data = epics_signal_r(Table, f"{prefix}:rdBPM")
+        super().__init__(name=name)
 
 
 if __name__ == "__main__":
     import asyncio
 
-    orbit = Orbit("ORBITCC:", name="orb")
+    # that's the advantage of the table ... just read what you get
+    # so could be sufficient to just read it in this manner
+    # but don't forget it to give it a name ... otherwise it
+    # ·uses an empty string
+    orbit_pv = epics_signal_r(Table, "pva://ORBITCC:rdBPM", name="orbit")
+
+    orbit = Orbit("pva://ORBITCC", name="orb")
 
     async def test():
-        await orbit.connect()
+        await asyncio.gather(orbit.connect(), orbit_pv.connect())
         r = await orbit.read()
-        print(r)
+        table = r["orb-data"]["value"]
+        print(table)
 
+        r = await orbit_pv.read()
+        table = r["orbit"]["value"]
+        print(table)
     asyncio.run(test())
