777d9157e31d158e8c649d9daa118ffdc7abb6b5
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-04-07 17:46:51 +0200

    [TASK] clean up of code

--- bact_bessyii_mls_ophyd/devices/pp/orbit.py
+++ bact_bessyii_mls_ophyd/devices/pp/orbit.py
@@ -1,48 +1,25 @@
-from dataclasses import asdict
 from typing import Dict
 
 from bluesky.protocols import Reading
 from event_model import DataKey
 
-from bact_device_models.devices.orbit import (
-    BPMButtons,
-    BPMPosition,
-    BPMReading,
-    Orbit as OrbitModel,
-)
 
 from ..raw.orbit import Orbit as ROrbit
-import json
 import numpy as np
 
 
 class Orbit(ROrbit):
-    """Provide read in data as orbit model"""
+    """Provide read in data as orbit model
+
+    Todo:
+        improve storage of normative table
+    """
 
     async def describe(self) -> dict[str, DataKey]:
         d = await super().describe()
-        # Table not json serializable
+        # split up info in table in individual columns
         tmp = d.pop(f"{self.name}-data")
         descr_w_str = table_bytes_to_str_dtype(tmp["dtype_numpy"])
-        descr_wo_str = table_dtype_without_str(tmp["dtype_numpy"])
-        d2 = {
-            # f"{self.name}-pos": DataKey(source=tmp["source"], shape=[], dtype="array"),
-            # Todo: need to describe the data properly so that it fits into the
-            # data model
-             f"{self.name}-data": DataKey(
-                  dtype="array",
-                  source=tmp["source"],
-                  shape=(len(descr_w_str),),# [len(descr_w_str)] + tmp["shape"],
-                  dtype_numpy=[(key, type) for key, type in descr_w_str],
-            ),
-            f"{self.name}-data-sel": DataKey(
-                  source=tmp["source"],
-                  shape=[len(descr_wo_str)] + tmp["shape"],
-                  dtype="array",
-                 dtype_numpy=descr_wo_str,
-             ),
-
-        }
         d3 = {
             f"{self.name}-{name}": DataKey(
                 dtype="array",
@@ -97,40 +74,6 @@ def table_bytes_to_str(table):
             tmp = np.asarray(tmp).astype("U")
         data[name] = tmp
     return data
-
-
-def table_without_str(table):
-    """
-    Todo:
-        should only uniform type be supported
-    """
-    dtype = table.numpy_dtype()
-    ndtype = table_dtype_without_str(dtype.descr)
-
-    data = np.empty([len(table)], dtype=ndtype)
-
-    for name, ttype in data.dtype.descr:
-        tmp = getattr(table, name)
-        if "U" in ttype:
-            tmp = np.asarray(tmp).astype("U")
-        data[name] = tmp
-    return data
-
-
-def table_dtype_without_str(descr):
-    def valid_type(ttype: str) -> bool:
-        if "S" in ttype:
-            return False
-        elif "U" in ttype:
-            return False
-        elif "i" in ttype:
-            return False
-        else:
-            return True
-
-    r = [(name, type) for name, type in descr if valid_type(type)]
-
-    return r
 
 
 __all__ = ["Orbit"]
